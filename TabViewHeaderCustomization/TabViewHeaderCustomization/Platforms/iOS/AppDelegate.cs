a0e0502c33752cedaf361b819233f9f258f176be
Kishore4299 <kishore.jeyachandran@syncfusion.com>
2026-01-02 17:53:13 +0530

    Updated the sample for TabView header customization.

---
+++ TabViewHeaderCustomization/TabViewHeaderCustomization/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TabViewHeaderCustomization
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
